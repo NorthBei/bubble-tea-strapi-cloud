1105a19125ee0ea0218268e17624b74be222f4d8
NorthBei <seekingfor520@gmail.com>
2025-06-14 12:02:11 +0800

    Add google storage supporting

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,15 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: '@strapi-community/strapi-provider-upload-google-cloud-storage',
+      providerOptions: {
+        bucketName: env('GCS_BUCKET_NAME'),
+        publicFiles: env.bool('GCS_PUBLIC_FILES', true),
+        uniform: env.bool('GCS_UNIFORM', false),
+        basePath: env('GCS_BASE_PATH', ''),
+        baseUrl: env('GCS_BASE_URL', `https://storage.googleapis.com/${env('GCS_BUCKET_NAME')}`),
+        serviceAccount: env.json('GCS_SERVICE_ACCOUNT'),
+      },
+    },
+  },
+});
